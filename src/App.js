590c4e1c38caec367fe6020c505a8b38776314c2
fahrul23 <ihsanfahrul23@gmail.com>
2021-06-23 15:11:07 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, {useState,useEffect} from 'react';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+    const [quote,setQuote] = useState('');
+    const [myquote,setMyQuote] = useState('');
+    const [favorite,setFavorite] = useState('');
+    const [onsubmit,setOnSubmit] = useState(false);
+
+    const getQuote = async()=>{
+            
+            let response = await axios.get('https://api.kanye.rest',{
+                headers:{
+                    'Content-Type' : 'aplication/json'  
+                }
+            });
+            setQuote(response.data.quote);
+        }
+    const changeQuote = ()=>{
+        getQuote();
+    }
+    const addFavorite = () =>{
+        setFavorite(quote);
+    }
+    const onSubmit =()=>{
+        setOnSubmit(true);
+    }
+    const setMyfavoritequote=(e)=>{
+        setMyQuote(e.target.value);
+        setOnSubmit(false);
+    }
+    useEffect(()=>{
+        getQuote();
+    },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img width="200" height="200" src="https://images.businessoffashion.com/profiles/asset/1797/43897e2e4a6d155d72dd9df352017b546ef9e229.jpeg" alt="img"/>
+      <h1>Kanye-West Quote</h1>
+      <p>{quote}</p>
+      <button onClick={()=>changeQuote()}>get Quote</button>
+      <button onClick={()=>addFavorite()}>add Favorite</button>
+      <p>{favorite}</p>
+      <h2>My Quote</h2>
+      <input type="text" onChange={(e) => setMyfavoritequote(e)} />
+      <button onClick={()=> onSubmit()} >Submit</button>
+      {onsubmit ?
+        (<p>{myquote}</p>)
+      : '' }
     </div>
   );
 }
